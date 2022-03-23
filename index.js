36c0179328e849c281c00b4376b8b554074b54c3
Faissoil22 <faissoil1@me.com>
2022-03-23 19:54:22 +0100

    Update index.html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function Page() {
                     <img src="./react-logo.png" width="100px" />
                 </nav>
             </header>
-            <div class="container">
+            <div class="containerpage">
                 <h1>Mes pronostics gratuits du weekend. </h1>
                 <p>
                     <strong>Ligue 1</strong>
@@ -18,11 +18,21 @@ function Page() {
                             <strong>St-Etienne VS Marseille, 2 Avril 2022 20:00</strong>
                         </li>
                         <img src="./images/ste_mar.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                         <p></p>
                         <li>
                             <strong>Paris SG VS Lorient, 3 Avril 2022 19:45</strong>
                         </li>
                         <img src="./images/psg_lor.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                     </li>
                 </ul>
                 <p>
@@ -34,11 +44,21 @@ function Page() {
                             <strong>FC Barcelone VS FC Séville, 3 Avril 2022 20:00</strong>
                         </li>
                         <img src="./images/bar_sev.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                         <p></p>
                         <li>
                             <strong>Real Sociedad VS Espanyol, 4 Avril 2022 20:00</strong>
                         </li>
                         <img src="./images/soc_esp.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                     </li>
                 </ul>
                 <p>
@@ -50,11 +70,21 @@ function Page() {
                             <strong>Salertina Vs Torino, 2 Avril 2022 19:45</strong>
                         </li>
                         <img src="./images/sal_tor.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                         <p></p>
                         <li>
                             <strong>AC Milan VS Bologna, 4 Avril 2022 19:45</strong>
                         </li>
                         <img src="./images/mil_bol.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                     </li>
                 </ul>
                 <p>
@@ -66,11 +96,22 @@ function Page() {
                             <strong>Tottenham VS Newcastle, 3 Avril 2022 16:30</strong>
                         </li>
                         <img src="./images/tot_new.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                            <p></p>
+                        </div>
                         <p></p>
                         <li>
                             <strong>Mantchester Utd VS Leicester, 2 Avril 2022 17:30</strong>
                         </li>
                         <img src="./images/man_lei.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                     </li>
                 </ul>
                 <p>
@@ -82,11 +123,21 @@ function Page() {
                             <strong>Dortmund VS Leipzig, 2 Avril 2022 17:30</strong>
                         </li>
                         <img src="./images/dor_leip.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                         <p></p>
                         <li>
                             <strong>Mgladbach VS Mayance, 3 Avril 2022 16:30</strong>
                         </li>
                         <img src="./images/mgl_may.png" />
+                        <div class="liens">
+                            <a target="_blank" href="https://www.amazon.fr/gp/search?ie=UTF8&tag=mespronosduwe-21&linkCode=ur2&linkId=1c46747561ea7e41e4cb538cd41c243f&camp=1642&creative=6746&index=books&keywords=Paris sportifs">
+                                Comment gagner aux paris sportifs
+                            </a>
+                        </div>
                     </li>
                 </ul>
                 <p>Mes pronostics se basent sur la comparaison des statistiques des matchs à domicile et à l'extérieur des deux équipes d'une rencontre. Sont prises en compte dans mes calculs:</p>
@@ -101,18 +152,6 @@ function Page() {
                     <li>Average corners</li>
                 </ul>
             </div>
-            <ul>
-                <footer>
-                    <p>
-                        <p>
-                            <small>
-                                <strong>Sachez que jouer aux jeux de hasard comporte des risques</strong>
-                            </small>
-                        </p>
-                        <small>(c) AliF 2022. Tous droits Réservés</small>
-                    </p>
-                </footer>
-            </ul>
         </div>
     );
 }
